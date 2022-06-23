c8d479d49af6cdfb7e8d6930ce6f8e8847960a47
Ontustik <108077792+Ontustik@users.noreply.github.com>
2022-06-23 21:36:45 +0700

    Update Calculate_the_value_of_the_trinomial.c

--- practiks/Calculate_the_value_of_the_trinomial.c
+++ practiks/Calculate_the_value_of_the_trinomial.c
@@ -1,1 +1,20 @@
-f
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int y(int a, int b, int c, int x)
+{
+	return a * (int)pow(x, 2) + b * x + c;
+}
+
+int main()
+{
+	int
+		a = 0,
+		b = 0,
+		c = 0,
+		x = 1;
+	printf("A B C X\n");
+	scanf("%d%d%d%d", &a, &b, &c, &x);
+	printf("\n%d", y(a, b, c, x));
+}
